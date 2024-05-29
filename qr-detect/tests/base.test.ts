5a20ec783094b2ee293fd0e0c32b0285295315ad
ahaoboy <504595380@qq.com>
2024-05-29 18:56:34 +0800

    add url test

--- qr-detect/tests/base.test.ts
+++ qr-detect/tests/base.test.ts
@@ -2,8 +2,20 @@ import { expect, test } from "vitest"
 import { detect } from "../src"
 import { readFileSync } from "node:fs"
 
-test("base", () => {
+test("text", () => {
   const buf = new Uint8Array(readFileSync("./assets/a.webp"))
   const r = detect(buf)
   expect(r).toEqual(["hello world"])
+})
+
+test("url", () => {
+  const buf = new Uint8Array(readFileSync("./assets/b.webp"))
+  const r = detect(buf)
+  expect(r).toEqual(["https://qr2clipboard.vercel.app/"])
+})
+
+test("text and url", () => {
+  const buf = new Uint8Array(readFileSync("./assets/c.webp"))
+  const r = detect(buf)
+  expect(r).toEqual(["https://qr2clipboard.vercel.app/", "hello world"])
 })
